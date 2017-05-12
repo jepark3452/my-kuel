720951173d2d30f2ff987ae1d1a02e8b0b4a3c1a
DESKTOP-MGMHCD5\jepark <jepark3452@gmail.com>
2017-05-12 13:34:53 +0900

    create project inital commit

---
+++ src/main/java/net/kuel/MyKuelApplication.java
@@ -0,0 +1,12 @@
+package net.kuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyKuelApplication.class, args);
+	}
+}
